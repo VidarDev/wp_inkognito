822ae69dbefb69ef5b6dd33019a97f8bc570eacf
trichier <>
2023-11-09 22:08:16 +0100

    #001 - Réecriture du code du footer

--- footer.php
+++ footer.php
@@ -16,24 +16,35 @@
 <!-- Contact -->
 <div class="contact">
     <div class="container">
-        <span class="contact__subtitle subtitle">
-            <?php if (get_field('footer__subtitle', 'option')): echo get_field('footer__subtitle', 'option'); endif; ?>
-        </span>
-        <h2 class="contact__title title">
-            <?php if (get_field('footer__title', 'option')): echo get_field('footer__title', 'option'); endif; ?>
-        </h2>
+
+        <?php if (get_field('footer__subtitle', 'option')): ?>
+            <span class="contact__subtitle subtitle">
+                <?= get_field('footer__subtitle', 'option'); ?>
+            </span>
+        <?php endif; ?>
+
+        <?php if (get_field('footer__title', 'option')): ?>
+            <h2 class="contact__title title">
+                <?= get_field('footer__title', 'option'); ?>
+            </h2>
+        <?php endif; ?>
+
         <div class="contact__wrapper">
             <div class="contact__image">
-                <div class="contact__vertical">
-                    <span>
-                        <?php if (get_field('footer__vertical-text', 'option')): echo get_field('footer__vertical-text', 'option'); endif; ?>
-                    </span>
-                </div>
+
+                <?php if (get_field('footer__vertical-text', 'option')): ?>
+                    <div class="contact__vertical">
+                        <span>
+                            <?= get_field('footer__vertical-text', 'option'); ?>
+                        </span>
+                    </div>
+                <?php endif; ?>
+
                 <?php $contact_image = get_field('footer__image', 'option'); ?>
                 <?php if ($contact_image && !empty($contact_image)): ?>
                     <figure>
                         <img src="<?= esc_url($contact_image['url']) ?>"
-                             role="presentation"
+                             role="img"
                              aria-label="<?= esc_url($contact_image['alt']) ?>"
                              alt="<?= esc_url($contact_image['alt']) ?>"
                              title="<?= esc_url($contact_image['alt']) ?>"
@@ -43,19 +54,33 @@
                         />
                     </figure>
                 <?php endif; ?>
+
             </div>
             <div class="contact__content">
-                <span class="contact__adresse-subtitle">
-                    <?php if (get_field('footer__address-subtitle', 'option')): echo get_field('footer__address-subtitle', 'option'); endif; ?>
-                </span>
-                <h3 class="contact__adresse-title">
-                    <?php if (get_field('footer__address-name', 'option')): echo get_field('footer__address-name', 'option'); endif; ?>
-                </h3>
-                <a href="<?php if (get_field('footer__address-url', 'option')): echo get_field('footer__address-url', 'option'); endif; ?>"
-                   target="_blank" class="contact__adresse">
-                    <i class="iconoir-position"></i>
-                    <p><?php if (get_field('footer__address', 'option')): echo get_field('footer__address', 'option'); endif; ?></p>
-                </a>
+
+                <?php if (get_field('footer__address-subtitle', 'option')): ?>
+                    <span class="contact__adresse-subtitle">
+                        <?= get_field('footer__address-subtitle', 'option'); ?>
+                    </span>
+                <?php endif; ?>
+
+                <?php if (get_field('footer__address-name', 'option')): ?>
+                    <h3 class="contact__adresse-title">
+                        <?= get_field('footer__address-name', 'option'); ?>
+                    </h3>
+                <?php endif; ?>
+
+                 <?php if (
+                     get_field('footer__address-url', 'option')
+                     && get_field('footer__address', 'option')):
+                 ?>
+                    <a href="<?= get_field('footer__address-url', 'option'); ?>"
+                       target="_blank" class="contact__adresse">
+                        <i class="iconoir-position"></i>
+                        <p><?= get_field('footer__address', 'option'); ?></p>
+                    </a>
+                <?php endif; ?>
+
                 <?php $contact_geoloc = get_field('footer__address-map', 'option'); ?>
                 <?php if ($contact_geoloc && !empty($contact_geoloc)): ?>
                     <figure class="contact__adresse-geolocation">
@@ -70,6 +95,7 @@
                         />
                     </figure>
                 <?php endif; ?>
+
             </div>
         </div>
     </div>
